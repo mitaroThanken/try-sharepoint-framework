e4ab6b4f3839d97889ce3e6d7eba8c02b0549b05
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-23 17:15:14 +0000

    Exercise - Create a SharePoint Framework client-side web part
    https://learn.microsoft.com/en-us/training/modules/sharepoint-spfx-web-parts/3-exercise-create-web-part

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -33,18 +33,15 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
         <p>
         The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
         </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
+        <button type="button">Show welcome message</button>
       </div>
     </section>`;
+
+    this.domElement.getElementsByTagName("button")[0]
+      .addEventListener('click', (event: MouseEvent) => {
+        event.preventDefault();
+        alert('Welcome to the SharePoint Framework!');
+      });
   }
 
   protected onInit(): Promise<void> {
